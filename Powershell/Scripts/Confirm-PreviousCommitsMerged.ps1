504d1a96f6f10ed6175eefdc4c555f0dd253e364
Mike Burgess <mburgess@cogstate.com>
2024-03-20 06:46:42 -0700

    adding some better search functionality to this powershell script.

--- Powershell/Scripts/Confirm-PreviousCommitsMerged.ps1
+++ Powershell/Scripts/Confirm-PreviousCommitsMerged.ps1
@@ -8,7 +8,29 @@ trap
     Write-Error "$($_.Exception)`n`nActual Stack Trace:`n$($_.ScriptStackTrace)`n`nError Output Stack Trace:" 
     exit 1
 }
-$modulepath = Get-ChildItem -recurse -Include Version-Control.psm1
-Import-Module $modulePath[0]
+
+$currentDirectory = Get-Location
+Write-Output "Current Directory is: $currentDirectory"
+
+$parentDirectory = Join-Path -Path $currentDirectory -ChildPath ".."
+Write-Output "Parent directory is: $parentDirectory"
+
+$modulePath = Get-ChildItem $parentDirectory -Recurse -Filter "Version-Control.psm1" | Select-Object -First 1
+if (-not $modulePath) {
+    Write-Output "Module path not found in current directory, going up a directory..."
+    $parentDirectory = Set-Location (Join-Path -Path $parentDirectory -ChildPath "..")
+    Write-Output "Parent directory is now: $parentDirectory"
+    $modulePath = Get-ChildItem $parentDirectory -Recurse -Filter "Version-Control.psm1" | Select-Object -First 1
+}
+
+if ($modulePath) {
+    Write-Output "Module path found: $($modulePath.FullName)"
+    Import-Module $modulePath.FullName
+} else {
+    Write-Output "Module path not found. Exiting..."
+    Write-Error "Error: Module path is empty."
+    exit 2
+}
 
 VerifyAllGitDirectories
+
