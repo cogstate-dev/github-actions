677216bc74ccfe65676468139b3184f74898fe28
Mike Burgess <mburgess@cogstate.com>
2024-05-16 16:04:16 -0700

    Adding a funciton to version-control.psm1 Calling the function in new script set-versions-json.ps1 Adding a composite workflow to call this. This is to create a versions file from the version variable.

---
+++ Powershell/Scripts/Set-Versions-Json.ps1
@@ -0,0 +1,42 @@
+[CmdletBinding()]
+param (
+    [parameter(Mandatory=$true)]
+    [String]
+    $Version
+)
+
+$WarningPreference = "Continue"
+$ErrorActionPreference = "Stop"
+$VerbosePreference = "Continue"
+$DebugPreference = "Continue"
+
+trap
+{
+    Write-Error "$($_.Exception)`n`nActual Stack Trace:`n$($_.ScriptStackTrace)`n`nError Output Stack Trace:" 
+    exit 1
+}
+
+$currentDirectory = Get-Location
+Write-Output "Current Directory is: $currentDirectory"
+
+$parentDirectory = Join-Path -Path $currentDirectory -ChildPath ".."
+Write-Output "Parent directory is: $parentDirectory"
+
+$modulePath = Get-ChildItem $parentDirectory -Recurse -Filter "Version-Control.psm1" | Select-Object -First 1
+if (-not $modulePath) {
+    Write-Output "Module path not found in current directory, going up a directory..."
+    $parentDirectory = Set-Location (Join-Path -Path $parentDirectory -ChildPath "..")
+    Write-Output "Parent directory is now: $parentDirectory"
+    $modulePath = Get-ChildItem $parentDirectory -Recurse -Filter "Version-Control.psm1" | Select-Object -First 1
+}
+
+if ($modulePath) {
+    Write-Output "Module path found: $($modulePath.FullName)"
+    Import-Module $modulePath.FullName
+} else {
+    Write-Output "Module path not found. Exiting..."
+    Write-Error "Error: Module path is empty."
+    exit 2
+}
+
+New-VersionsJson -version $Version
